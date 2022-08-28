27b11bdfc88e5d0616f017735dc682b21a7b6646
Tuantrung <tuantrung03121997@gmail.com>
2022-08-28 23:32:52 +0700

    Done number line jump problem

---
+++ Implementation/grading-students.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+vector<int> gradingStudents(vector<int> grades) {
+    vector<int> output;
+
+    for(int i=0; i<grades.size();i++){
+        if(grades[i] < 38){
+            output.push_back(grades[i]);
+            continue;
+        }
+
+        int tmp = grades[i]/5;
+        if((tmp+1)*5 - grades[i] < 3){
+            grades[i] = (tmp+1)*5;
+            output.push_back(grades[i]);
+        }
+        else
+            output.push_back(grades[i]);
+    }
+
+    return output;
+}
+
+int main()
+{
+    // ofstream fout(getenv("OUTPUT_PATH"));
+
+    string grades_count_temp;
+    getline(cin, grades_count_temp);
+
+    int grades_count = stoi(ltrim(rtrim(grades_count_temp)));
+
+    vector<int> grades(grades_count);
+
+    for (int i = 0; i < grades_count; i++) {
+        string grades_item_temp;
+        getline(cin, grades_item_temp);
+
+        int grades_item = stoi(ltrim(rtrim(grades_item_temp)));
+
+        grades[i] = grades_item;
+    }
+
+    vector<int> result = gradingStudents(grades);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        // fout << result[i];
+        cout << result[i];
+
+        if (i != result.size() - 1) {
+            // fout << "\n";
+            cout << "\n";
+
+        }
+    }
+
+    // fout << "\n";
+    cout << "\n";
+
+
+    // fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
